a1e21f7880db5b3765c847de515b369f86ce9fcc
krish joshi <krishjoshi2407@gmail.com>
2026-06-03 19:08:45 +0530

    fix ecommerce link path

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -12,9 +12,9 @@ function Header(){
                 <li><Link to="/login">Login</Link></li>
                 <li><Link to="/#">Contact</Link></li>
                 <li><Link to="/#">About</Link></li>
-               <a href="/model_1/model1-sataticweb/index.html">
-  E-commerce website
-</a>
+                <a href="/model_1/model1-sataticweb/index.html">
+                 E-commerce website
+                </a>
             </ul>
         </nav>
 
