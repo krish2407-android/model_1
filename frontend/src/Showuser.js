20080055c2bc317760e535911d178feb8083607b
krish joshi <krishjoshi2407@gmail.com>
2026-06-04 13:14:03 +0530

    all work done

--- frontend/src/Showuser.js
+++ frontend/src/Showuser.js
@@ -1,44 +1,50 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "./Header";
-// import register from "../../backend/db/register";
+import "./Showuser.css";
 
 function Showuser() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:2407/api/register")
-.then((res) => {
-  setUsers(res.data.data);
-});
+    axios
+      .get("http://localhost:2407/api/register")
+      .then((res) => {
+        setUsers(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
     <>
       <Header />
 
-      <div style={{ padding: "20px" }}>
-        <h1>MongoDB Users Data</h1>
+      <div className="showuser-container">
+        <h1 className="showuser-title">MongoDB Users Data</h1>
 
-        <table border="1" cellPadding="10" style={{ width: "100%" }}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Password</th>
-            </tr>
-          </thead>
+        <div className="table-container">
+          <table className="user-table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+              </tr>
+            </thead>
 
-          <tbody>
-           {users.map((register) => (
-  <tr key={register._id}>
-    <td>{register.name}</td>
-    <td>{register.email}</td>
-    <td>{register.password}</td>
-  </tr>
-))}
-          </tbody>
-        </table>
+            <tbody>
+              {users.map((register) => (
+                <tr key={register._id}>
+                  <td>{register.name}</td>
+                  <td>{register.email}</td>
+                  <td>{register.password}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
